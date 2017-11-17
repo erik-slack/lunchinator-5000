c3a2125ec21ab41d36fabd5f4aaeffbe25219d3f
Erik Slack <erik.slack@stgconsulting.com>
2017-11-16 17:32:09 -0700

    finished logic for suggestion

--- models/ballot.js
+++ models/ballot.js
@@ -1,5 +1,6 @@
 var uuidv4 = require('uuid/v4');
 var _ = require('lodash');
+var utils = require('../utils');
 
 module.exports = {
     Ballot: Ballot
@@ -14,10 +15,16 @@ function Ballot(ballotData) {
     this.winner = null;
     this.suggestion = null;
     this.getContestedBallot = function () {
-        return _.pick(this, 'suggestion', 'choices');
+        result = _.pick(this, 'suggestion', 'choices');
+        result.choices = utils.shuffle(result.choices);
+        return result;
     };
     this.getWonBallot = function () {
         return _.pick(this, 'winner', 'choices');
+    };
+    this.hasExpired = function () {
+        // todo: write logic for this
+        return false;
     };
 }
 
--- routes/ballot.js
+++ routes/ballot.js
@@ -18,6 +18,7 @@ module.exports = function () {
 
 /*// Session Data //*/
 
+var NUM_OF_CHOICES = 5;
 var ballots = [];
 
 /*// WSMs //*/
@@ -25,23 +26,82 @@ var ballots = [];
 function createBallot(req, res) {
     var newBallot = new Ballot(req.body);
 
-    getRestaurants()
-        .then(function(data) {
-            utils.pickRandomChoices(newBallot.choices, data, 5);
-            ballots.push(newBallot);
+    addChoices(newBallot, addSuggestion);
+
+    function addChoices() {
+        getRestaurants()
+            .then(function(data) {
+                utils.pickRandomChoices(newBallot.choices, data, NUM_OF_CHOICES);
+                populateChoicesWithAverageReviews();
+            });
+    }
+
+    function populateChoicesWithAverageReviews() {
+        var responseCount = 0;
+        var reviewsMerged = [];
+        for (var i = 0; i < NUM_OF_CHOICES; i++) {
+            getReview(newBallot.choices[i].name, i)
+                .then(function(result) {
+                    newBallot.choices[result.iterator].averageReview = calculateAverageRating(newBallot.choices[result.iterator], result.data);
+                    reviewsMerged = reviewsMerged.concat(result.data);
+                    responseCount++;
+                    if (responseCount === NUM_OF_CHOICES) {
+                        addSuggestion(reviewsMerged);
+                    }
+                });
+        }
+    }
+
+    function calculateAverageRating(choiceToUpdate, reviews) {
+        var ratingsArray = [];
+        var numOfReviews = reviews.length;
+        for (var j = 0; j < numOfReviews; j++) {
+            ratingsArray.push(parseInt(reviews[j].rating));
+        }
+        return _.round(_.mean(ratingsArray), 2);
+    }
+
+    function addSuggestion(relevantReviews) {
+        var highestRatedChoiceIndex = 0;
+        var highestRatingReviewIndex = -1;
+        for (var i = 1; i < NUM_OF_CHOICES; i++) {
+            var thisChoice = newBallot.choices[i];
+            var previousChoice = newBallot.choices[i - 1];
+            if (thisChoice.averageReview > previousChoice.averageReview) {
+                highestRatedChoiceIndex = i;
+            }
+        }
+        var reviewsCount = relevantReviews.length;
+        for (var i = 0; i < reviewsCount; i++) {
+            if (relevantReviews[i].restaurant === newBallot.choices[highestRatedChoiceIndex].name) {
+                if (highestRatingReviewIndex === -1 || (parseInt(relevantReviews[i].rating) > parseInt(relevantReviews[highestRatingReviewIndex].rating))) {
+                    highestRatingReviewIndex = i;
+                }
+            }
+        }
+        newBallot.suggestion = _.pick(newBallot.choices[highestRatedChoiceIndex], 'id', 'name', 'averageReview');
+        newBallot.suggestion.topReviewer = relevantReviews[highestRatingReviewIndex].reviewer;
+        newBallot.suggestion.review = relevantReviews[highestRatingReviewIndex].review;
+        onBallotCompleted();
+    }
+
+    function onBallotCompleted() {
+        ballots.push(newBallot);
+        res.send({
+            ballotId: newBallot.ballotId
         });
-    
-    res.send({
-        ballotId: newBallot.ballotId
-    });
+    }
 }
 
 function getBallotById(req, res) {
     // todo: if no ballotId param throw error
-    res.send({
-        wonBallot: newBallot.getWonBallot()
-    });
-    
+    var relevantBallot = null;
+    if (relevantBallot.hasExpired()) {
+        relevantBallot = newBallot.getContestedBallot();
+    } else {
+        relevantBallot = newBallot.getWonBallot();
+    }
+    res.send();
 }
 
 function getAllBallots(req, res) {
@@ -63,9 +123,30 @@ function getRestaurants() {
 
 function getRestaurant(name) {
     var deferred = q.defer();
-    routeUtils.simplifiedHttpsGet('restaurants/' + name, onEndFunction, routeUtils.defaultOnErrFunction, routeUtils.defaultOnNon200Code);
+    routeUtils.simplifiedHttpsGet('restaurants/' + encodeURIComponent(name), onEndFunction, routeUtils.defaultOnErrFunction, routeUtils.defaultOnNon200Code);
     function onEndFunction (data) {
         deferred.resolve(data);
+    }
+    return deferred.promise;
+}
+
+function getReviews(name) {
+    var deferred = q.defer();
+    routeUtils.simplifiedHttpsGet('reviews', onEndFunction, routeUtils.defaultOnErrFunction, routeUtils.defaultOnNon200Code);
+    function onEndFunction (data) {
+        deferred.resolve(JSON.parse(data));
+    }
+    return deferred.promise;
+}
+
+function getReview(name, iterator) {
+    var deferred = q.defer();
+    routeUtils.simplifiedHttpsGet('reviews/' + encodeURIComponent(name), onEndFunction, routeUtils.defaultOnErrFunction, routeUtils.defaultOnNon200Code);
+    function onEndFunction (data) {
+        deferred.resolve({
+            data: JSON.parse(data),
+            iterator: iterator
+        });
     }
     return deferred.promise;
 }
